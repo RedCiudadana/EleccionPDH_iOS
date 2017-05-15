27a13c4e41d9039ec82196816a02d61be5898942
Jeffry Turcios <jefoutta@Jeffrys-MacBook-Pro.local>
2017-05-14 18:17:00 -0600

    Initial Commit

---
+++ EleccionesPDH/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EleccionesPDH
+//
+//  Created by Jeffry Turcios on 5/14/17.
+//  Copyright © 2017 Codifay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EleccionesPDH/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  EleccionesPDH
+//
+//  Created by Jeffry Turcios on 5/14/17.
+//  Copyright © 2017 Codifay. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
